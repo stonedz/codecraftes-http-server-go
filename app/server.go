792816cdd999d1faa7f0068ec6af4b4220f39ad5
Paolo Fagni <paolo.fagni@gmail.com>
2024-05-14 22:25:11 +0200

    respond to a valid request

--- app/server.go
+++ app/server.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -25,6 +23,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buff := make([]byte, 1024)
+	n, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	data := string(buff[:n])
+
+	lines := strings.Split(data, "\r\n")
+
+	words := strings.Split(lines[0], " ")
+	target := words[1]
+
+	if target == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
 }
